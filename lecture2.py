19ad406377ef0ff203e38a368e6ee4ee3dca8a41
jerry-ban <jlban@hotmail.com>
2018-02-19 17:00:03 -0500

    init

---
+++ lecture2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt.interactive(True)
+
+mpl.get_backend()
+
+plt.plot?
+plt.plot(3, 2)
+plt.plot(3, 2, ".")
+
+
+
+plt.show()
+
+# matplotlib usually has 2 layers: backend layer, artist layer
+# scripting layer is to evaluate specialist: simplifies access to the artist and backend layers.
+
